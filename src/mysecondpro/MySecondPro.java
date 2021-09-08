2974da254e63f0cb5dbd086dc6788124541b74f9
pupil <pupil@ivkhk.ee>
2021-09-08 09:10:52 +0300

    First Change

--- src/mysecondpro/MySecondPro.java
+++ src/mysecondpro/MySecondPro.java
@@ -18,6 +18,11 @@ public class MySecondPro {
         System.out.print("Hello World2");
         System.out.print("First Update2");
         System.out.print("Second Change");
+        System.out.printf(String.join ("%n",
+                "Using argument index:",
+                "First number:%1$-5d",
+                "Second number:%2$5d",
+                "PI:%3$.5f%n"), 1234, 456, Math.PI);
         // TODO code application logic here
     }
     
